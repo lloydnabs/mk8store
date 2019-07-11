1560c88f3b54dc5f52890aa58d47f5e5be87467f
Lloyd Nabbit <lloydnabs@gmail.com>
2019-07-11 21:15:17 +0100

    just need to confim

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 #if defined(__WIIU__)
 #include <unistd.h>
 #endif
-#if OSGetTitleID==0x000500101004A100
-#define DEFAULT_REPO "http://host.ctgpu.tk/CTGPU/USA"
-#else OSGetTitleID = 0x000500101004A200
+#if OSGetTitleID==0x000500101004A200
 #define DEFAULT_REPO "http://host.ctgpu.tk/CTGPU/PAL"
+#else OSGetTitleID = 0x000500101004A100
+#define DEFAULT_REPO "http://host.ctgpu.tk/CTGPU/USA"
 #endif
 #include "libget/src/Get.hpp"
 #include "libget/src/Utils.hpp"
