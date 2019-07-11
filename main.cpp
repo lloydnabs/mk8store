c8fde726900ebefb402c2fdfd0490560f1216588
lloydnabs <32464813+lloydnabs@users.noreply.github.com>
2019-07-12 00:54:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #endif
 #include "dynamic_libs/os_functions.h"
 #include "dynamic_libs/sys_functions.h"
-#if OSGetTitleID()==0000500101004A200
+#if OSGetTitleID()==0x000500101004A200
 #define DEFAULT_REPO "http://host.ctgpu.tk/CTGPU/PAL"
 #else OSGetTitleID = 0x000500101004A100
 #define DEFAULT_REPO "http://host.ctgpu.tk/CTGPU/USA"
