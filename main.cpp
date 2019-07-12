4ee198d076aa5a29ea5fb81b5dec3ee5aa2ee996
lloydnabs <32464813+lloydnabs@users.noreply.github.com>
2019-07-12 19:27:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,18 +29,17 @@ int main(int argc, char* argv[])
         if (stat(ELF_PATH, &sbuff) == 0)
 	        std::rename(ELF_PATH,RPX_PATH); 
     #endif
+#if defined(__WIIU__) 
 	init_networking();
 	// create main get object
         uint64_t title_id = *(uint64_t*)0x10013C10;
         if (title_id == 0x000500101004A200)
 	        char* DEFAULT_REPO=(char*)("http://host.ctgpu.tk/CTGPU/PAL") ;
-	        Get* get = new Get("./.get/", DEFAULT_REPO);
-
         else
 	        char* DEFAULT_REPO=(char*)("http://host.ctgpu.tk/CTGPU/USA") ;
-	        Get* get = new Get("./.get/", DEFAULT_REPO);
+	Get* get = new Get("./.get/", DEFAULT_REPO);
 
-
+#endif
 #if defined(NOGUI)
 	// if NOGUI variable defined, use the console's main method
 	int console_main(Get*);
